0d21116bccb3957df2babbb47535489dfcbbaa1b
Azriel <thomas.domine-@epitech.eu>
2017-03-18 22:37:41 +0100

    ca me fait chier cette merde qui veut pas marcher bite ++ 42 blblblblblbl ezfuyhzeruigsy_o

--- src/down_tetrimino.c
+++ src/down_tetrimino.c
@@ -5,7 +5,7 @@
 ** Login   <thomas.domine-@epitech.eu>
 **
 ** Started on  Sat Mar 18 13:57:14 2017 Thomas Dominé
-** Last update Sat Mar 18 22:23:11 2017 Thomas Dominé
+** Last update Sat Mar 18 22:31:42 2017 Thomas Dominé
 */
 
 #include "../include/my.h"
@@ -44,7 +44,7 @@ void		move_down(t_data *data, int i)
 	}
 }
 
-/*void		down_tetrimino(t_data *data, t_all *all)
+void		down_tetrimino(t_data *data, t_all *all)
 {
 	int		i;
 	int		t;
@@ -53,7 +53,7 @@ void		move_down(t_data *data, int i)
 	t = 0;
 	i = count(data);
 	t = time(NULL) - data->time_down;
-	if (t == data->temp_down + 1)
+	if (t == data->temp_down + 1 || getch() == 's')
 	{
 		data->temp_down += 1;
 		if (verif_char_down(data) == 1)
@@ -64,9 +64,9 @@ void		move_down(t_data *data, int i)
 		else
 		move_down(data, i);
 	}
-}*/
+}
 
-void		down_tetrimino(t_data *data, t_all *all)
+/*void		down_tetrimino(t_data *data, t_all *all)
 {
 	int		i;
 	int		t;
@@ -88,4 +88,4 @@ void		down_tetrimino(t_data *data, t_all *all)
 	else
 	move_down(data, i);
 	}
-}
+}*/
